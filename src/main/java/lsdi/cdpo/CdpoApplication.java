31ff472facfae27b70fd1d9cecf5aef1a905830c
Vitor Santos <vitoralannl@gmail.com>
2023-03-14 16:39:35 -0300

    Initial commit

---
+++ src/main/java/lsdi/cdpo/CdpoApplication.java
@@ -0,0 +1,13 @@
+package lsdi.cdpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lsdi/cdpo/CdpoApplicationTests.java
@@ -0,0 +1,13 @@
+package lsdi.cdpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
